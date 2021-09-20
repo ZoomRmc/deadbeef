22c98b2ce651ae3662905e8dd954b9b109a5435f
Jakub Wasylków <kuba_160@protonmail.com>
2021-09-20 23:48:02 +0200

    Apply requested changes

--- main.c
+++ main.c
@@ -397,44 +397,37 @@ server_exec_command_line (const char *cmdline, int len, char *sendback, int sbsi
             parg_len--;
 
             const char *plugid = parg + strlen("--plugin=");
-            DB_plugin_t ** plugins = plug_get_list();
-            int i = 0;
-            while (plugins[i]) {
-                if (strcmp(plugins[i]->id, plugid) == 0) {
-                    if (plugins[i]->exec_cmdline != NULL) {
-                        FILE *fp = tmpfile();
-                        if (!fp) {
-                            trace_err ("Creating tmpfile failed: %s\n", strerror(errno));
-                            return -1;
-                        }
-                        int fd = fileno (fp);
-                        size_t plugarg_len = parg_len - strlen(parg) - 1;
-                        int ret = plugins[i]->exec_cmdline(parg + strlen(parg) + 1, (int) plugarg_len, fd);
-
-                        off_t out_size = lseek(fd, 0, SEEK_END);
-                        // copy plugin output to sendback (if any output produced)
-                        if ((out_size > 0) && sendback) {
-                            fflush (fp);
-                            rewind (fp);
-                            sendback[0]='\1';
-                            size_t bytes_read = fread (sendback + 1, 1, sbsize - 1, fp);
-                            if (bytes_read > 0) {
-                                sendback[bytes_read] = '\0';
-                            }
-                            else {
-                                trace_err ("Reading tmpfile failed\n");
-                                return -1;
-                            }
-                        }
-                        fclose (fp);
-                        if (ret) {
-                            // TODO have specific error codes sent to client?
-                            sendback[0] = '\2';
-                        }
-                    }
-                    break;
+            DB_plugin_t *p = plug_get_for_id(plugid);
+            if (p && p->api_vmajor == 1 && p->api_vminor >= 15 && p->exec_cmdline != NULL) {
+                FILE *fp = tmpfile();
+                if (!fp) {
+                    trace_err ("Creating tmpfile failed: %s\n", strerror(errno));
+                    return -1;
                 }
-                i++;
+                int fd = fileno (fp);
+                size_t plugarg_len = parg_len - strlen(parg) - 1;
+                int ret = p->exec_cmdline(parg + strlen(parg) + 1, (int) plugarg_len, fd);
+
+                off_t out_size = lseek(fd, 0, SEEK_END);
+                // copy plugin output to sendback (if any output produced)
+                if ((out_size > 0) && sendback) {
+                    fflush (fp);
+                    rewind (fp);
+                    sendback[0]='\1';
+                    size_t bytes_read = fread (sendback + 1, 1, sbsize - 1, fp);
+                    if (bytes_read > 0) {
+                        sendback[bytes_read] = '\0';
+                    }
+                    else {
+                        trace_err ("Reading tmpfile failed\n");
+                        return -1;
+                    }
+                }
+                fclose (fp);
+                if (ret) {
+                    // TODO have specific error codes sent to client?
+                    sendback[0] = '\2';
+                }
             }
             parg += parg_len + 1;
             continue;
